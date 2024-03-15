58e49a5e8cd49ae3b52e0f07a8277a24286c2232
Giuseppe Spina <giuseppespina@192.168.88.29>
2024-03-15 23:04:22 +0100

    first-release-fix-api

--- projects/ngx-custom-ng-select/src/public-api.ts
+++ projects/ngx-custom-ng-select/src/public-api.ts
@@ -3,5 +3,6 @@
  */
 
 export * from './lib/ngx-custom-ng-select.service';
-export * from './lib/ngx-custom-ng-select.component';
+export * from './lib/components/ngx-custom-ng-select/ngx-custom-ng-select.component';
 export * from './lib/ngx-custom-ng-select.module';
+export * from './lib/directives/show-if-truncated.directive';
